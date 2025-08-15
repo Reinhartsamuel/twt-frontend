583bae172ec2609d3fb6a35350f498fba1c9806b
Reinhartsamuel <smuel.rr@gmail.com>
2025-08-15 13:08:10 +0800

    fix: change process.env to import.meta.env

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,3 +1,3 @@
 export const BASE_URL:string = window.location.hostname === 'localhost' ?
-  process.env.VITE_BACKEND_BASE_URL_LOCAL! :
-  process.env.VITE_BACKEND_BASE_URL!;
+  meta.import.env.VITE_BACKEND_BASE_URL_LOCAL! :
+  meta.import.env.VITE_BACKEND_BASE_URL!;
