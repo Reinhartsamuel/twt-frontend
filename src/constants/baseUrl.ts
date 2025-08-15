8485433cd7213287098e95fb6db3e9bc3ee1106b
Reinhartsamuel <smuel.rr@gmail.com>
2025-08-15 13:15:27 +0800

    add logging

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,3 +1,4 @@
+console.log(`hostname: ${window.location.hostname}`)
 export const BASE_URL:string = window.location.hostname === 'localhost' ?
-  meta.import.env.VITE_BACKEND_BASE_URL_LOCAL! :
-  meta.import.env.VITE_BACKEND_BASE_URL!;
+import.meta.env.VITE_BACKEND_BASE_URL_LOCAL! :
+import.meta.env.VITE_BACKEND_BASE_URL!;
